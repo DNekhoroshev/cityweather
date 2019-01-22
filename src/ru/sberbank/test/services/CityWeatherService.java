3c5f2d0559496239cecf7df98e58d2fd164ce6cf
Nekhoroshev.D.Al <DANekhoroshev.SBT@sberbank.ru>
2019-01-22 19:52:32 +0300

    Update

--- src/ru/sberbank/test/services/CityWeatherService.java
+++ src/ru/sberbank/test/services/CityWeatherService.java
@@ -1,43 +1,72 @@
 package ru.sberbank.test.services;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-@Path("/byname")
+@Path("/temperature")
 public class CityWeatherService {
 	
 	@GET
-	@Produces("application/json")
-	public Response convertFtoC() throws JSONException {
- 
-		JSONObject jsonObject = new JSONObject();
-		Double fahrenheit = 98.24;
-		Double celsius;
-		celsius = (fahrenheit - 32) * 5 / 9;
-		jsonObject.put("F Value", fahrenheit);
-		jsonObject.put("C Value", celsius);
- 
-		String result = "@Produces(\"application/json\") Output: \n\nF to C Converter Output: \n\n" + jsonObject;
+	@Produces("text/plain")
+	public Response convertFtoC() throws JSONException { 
+		String result = "Correct calling: <host:port>/<context-path>/api/temperature/bycityname?name=<name>";
 		return Response.status(200).entity(result).build();
 	}
 	
-	@Path("/n")
+	@Path("/bycity")
 	@GET
 	@Produces("application/json")
-	public Response getTempByCityName(@QueryParam("name") float f) throws JSONException {
- 
-		JSONObject jsonObject = new JSONObject();
-		float celsius;
-		celsius = (f - 32) * 5 / 9;
-		jsonObject.put("F Value", f);
-		jsonObject.put("C Value", celsius); 
+	public Response getTempByCityName(@QueryParam("cityName") String city) throws JSONException { 
+		
+		if(city==null||city.isEmpty()){
+			return Response.status(400).entity("Empty city name is not allowed").build();
+		}
+		
+		JSONObject jsonObject = new JSONObject();	
+		jsonObject.put("temperature", 666);		
 		String result = jsonObject.toString();
 		return Response.status(200).entity(result).build();
+	}	
+	
+	@POST
+	@Produces("text/plain")
+	public Response doPost() throws JSONException {			
+		return Response.status(405).entity("Not allowed").build();
 	}
+	
+	@PUT
+	@Produces("text/plain")
+	public Response doPut() throws JSONException {			
+		return Response.status(405).entity("Not allowed").build();
+	}
+	
+	@DELETE
+	@Produces("text/plain")
+	public Response doDelete() throws JSONException {			
+		return Response.status(405).entity("Not allowed").build();
+	}
+	
+	@HEAD
+	@Produces("text/plain")
+	public Response doHead() throws JSONException {			
+		return Response.status(405).entity("Not allowed").build();
+	}
+	
+	@OPTIONS
+	@Produces("text/plain")
+	public Response doOptions() throws JSONException {			
+		return Response.status(405).entity("Not allowed").build();
+	}
+	
 }
